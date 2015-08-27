452cde60c4b8951bbc9a34006e7c7a9e0c221ef3
Islam Mustafa <islam@integreight.com>
2015-08-27 12:55:26 +0200

    Edits to the sample application

--- myapplication/src/main/java/com/integreight/onesheeld/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/integreight/onesheeld/myapplication/MainActivity.java
@@ -6,70 +6,124 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.integreight.onesheeld.sdk.OneSheeldConnectionCallback;
+import com.integreight.onesheeld.sdk.OneSheeldDataCallback;
 import com.integreight.onesheeld.sdk.OneSheeldDevice;
 import com.integreight.onesheeld.sdk.OneSheeldError;
 import com.integreight.onesheeld.sdk.OneSheeldErrorCallback;
 import com.integreight.onesheeld.sdk.OneSheeldScanningCallback;
 import com.integreight.onesheeld.sdk.OneSheeldSdk;
+import com.integreight.onesheeld.sdk.ShieldFrame;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-    TextView textView;
     Button button;
-
+    Spinner spinner;
+    private ArrayList<String> devices;
+    private ArrayList<OneSheeldDevice> oneSheeldDevices;
     public void onClick(View v) {
         OneSheeldSdk.getManager().setScanningTimeOut(20);
+        OneSheeldSdk.getManager().cancelScanning();
+        devices.clear();
+        oneSheeldDevices.clear();
+        devices.add("Nothing Selected");
         OneSheeldSdk.getManager().scan();
     }
 
+    public void onClickBroadcastOn(View v){
+        ShieldFrame sf=new ShieldFrame((byte)0x03,(byte)0x01);
+        sf.addArgument(true);
+        OneSheeldSdk.getManager().broadcastShieldFrame(sf);
+    }
+
+    public void onClickBroadcastOff(View v){
+        ShieldFrame sf=new ShieldFrame((byte)0x03,(byte)0x01);
+        sf.addArgument(false);
+        OneSheeldSdk.getManager().broadcastShieldFrame(sf);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.hello_world);
+        spinner= (Spinner) findViewById(R.id.spinner);
         button = (Button) findViewById(R.id.button);
+        devices=new ArrayList<>();
+        oneSheeldDevices=new ArrayList<>();
+        devices.add("Nothing Selected");
+        final ArrayAdapter<String> devicesArrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,devices);
+        spinner.setAdapter(devicesArrayAdapter);
+        spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) return;
+                Log.d("MainActivity", devices.get(position));
+                OneSheeldSdk.getManager().cancelScanning();
+                oneSheeldDevices.get(position - 1).connect();
+                oneSheeldDevices.get(position - 1).addDataCallback(new OneSheeldDataCallback() {
+                    @Override
+                    public void onShieldFrameReceive(ShieldFrame frame) {
+                        Log.d("MainActivity","Frame:"+frame.getArgumentAsString(0));
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         OneSheeldSdk.init(this);
         OneSheeldSdk.getManager().setRetryCount(4);
-//        OneSheeldSdk.getManager().disableDefaultConnectingRetries();
+        OneSheeldSdk.setDebugging(true);
+        OneSheeldSdk.getManager().setAutomaticConnectingRetries(true);
 
         OneSheeldSdk.getManager().addCallbacks(new OneSheeldScanningCallback() {
             @Override
-            public void onStartScan() {
-                Log.d("MainActivity", "Scanning Started");
+            public void onScanStart() {
+//                Log.d("MainActivity", "Scanning Started");
+
             }
 
             @Override
             public void onDeviceFind(OneSheeldDevice device) {
-                Log.d("MainActivity", "Device Found: " + device.getName());
+//                Log.d("MainActivity", "Device Found: " + device.getName());
+                oneSheeldDevices.add(device);
+                devices.add(device.getName());
+                devicesArrayAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onFinishScan(List<OneSheeldDevice> foundDevices) {
-                Log.d("MainActivity", "Finished Scanning: " + foundDevices.size() + " device(s) found");
-                for (OneSheeldDevice device : foundDevices) {
-                    OneSheeldSdk.getManager().connect(device);
-                }
+            public void onScanFinish(List<OneSheeldDevice> foundDevices) {
+//                Log.d("MainActivity", "Finished Scanning: " + foundDevices.size() + " device(s) found");
+//                for (OneSheeldDevice device : foundDevices) {
+//                    OneSheeldSdk.getManager().connect(device);
+////                    OneSheeldSdk.getKnownShields().co;
+//
+//                }
             }
         }, new OneSheeldConnectionCallback() {
             @Override
             public void onConnect(OneSheeldDevice device) {
-                Log.d("MainActivity", "Connected To: " + device.getName());
+//                Log.d("MainActivity", "Connected To: " + device.getName());
             }
 
             @Override
             public void onDisconnect(OneSheeldDevice device) {
-                Log.d("MainActivity", "Disconnected From: " + device.getName());
+//                Log.d("MainActivity", "Disconnected From: " + device.getName());
             }
 
             @Override
             public void onConnectionRetry(OneSheeldDevice device, int retryCount) {
-                Log.d("MainActivity", "Retried Connection To: " + device.getName()+", Retry #"+retryCount);
+//                Log.d("MainActivity", "Retried Connection To: " + device.getName()+", Retry #"+retryCount);
 //                OneSheeldSdk.getManager().cancelConnecting();
             }
         }, new OneSheeldErrorCallback() {
