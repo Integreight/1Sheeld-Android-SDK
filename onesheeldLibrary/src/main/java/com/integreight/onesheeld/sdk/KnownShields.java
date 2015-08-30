e6bb22dc5e6d14752956ab0cab4ca52cc82f09af
Islam Mustafa <islam@integreight.com>
2015-08-30 13:16:55 +0200

    Added the license headers and file

--- onesheeldLibrary/src/main/java/com/integreight/onesheeld/sdk/KnownShields.java
+++ onesheeldLibrary/src/main/java/com/integreight/onesheeld/sdk/KnownShields.java
@@ -1,3 +1,19 @@
+/*
+* This code is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License version 3 only, as
+* published by the Free Software Foundation.
+*
+* This code is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+* version 3 for more details (a copy is included in the LICENSE file that
+* accompanied this code).
+*
+* Please contact Integreight, Inc. at info@integreight.com or post on our
+* support forums www.1sheeld.com/forum if you need additional information
+* or have any questions.
+*/
+
 package com.integreight.onesheeld.sdk;
 
 import java.util.ArrayList;
