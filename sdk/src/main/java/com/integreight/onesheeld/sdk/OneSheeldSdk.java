a26fcaec157c4e7a567773438ca8c2c908cde295
Islam Mustafa <islam@integreight.com>
2015-11-08 16:35:51 +0200

    Bumped up the versions of the compatible library and firmware versions

--- sdk/src/main/java/com/integreight/onesheeld/sdk/OneSheeldSdk.java
+++ sdk/src/main/java/com/integreight/onesheeld/sdk/OneSheeldSdk.java
@@ -24,8 +24,8 @@ import android.content.Context;
  */
 public class OneSheeldSdk {
     static final String TAG = "OneSheeldSdk";
-    private static final FirmwareVersion compatibleFirmwareVersion = new FirmwareVersion(1, 3);
-    private static final int compatibleLibraryVersion = 7;
+    private static final FirmwareVersion compatibleFirmwareVersion = new FirmwareVersion(1, 5);
+    private static final int compatibleLibraryVersion = 9;
     private static final int versionCode = BuildConfig.VERSION_CODE;
     private static final String versionName = BuildConfig.VERSION_NAME;
     private static boolean isDebuggingEnabled = false;
