bf995c8606fadbd72b844a0cf37375e0fa7dbdb1
Islam Mustafa <islam@integreight.com>
2016-03-16 15:39:33 +0200

    Removed a unused import

--- sampleApplication/src/main/java/com/integreight/onesheeld/sampleapplication/MainActivity.java
+++ sampleApplication/src/main/java/com/integreight/onesheeld/sampleapplication/MainActivity.java
@@ -33,7 +33,6 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 
